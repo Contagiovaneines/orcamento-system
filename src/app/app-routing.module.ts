6ed78a1c9f307fbfb47cb60893780de7d81553a0
Contagiovaneines <contatogiovaneines@gmail.com>
2025-01-11 18:10:30 -0300

    Primeiro commit - Enviando o projeto para o GitHub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { ListaOrcamentosComponent } from './lista-orcamentos/lista-orcamentos.component';
+import { NovoOrcamentoComponent } from './novo-orcamento/novo-orcamento.component';
+import { DetalheOrcamentoComponent } from './detalhe-orcamento/detalhe-orcamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent }, // Dashboard como padrão
+  { path: 'orcamentos/tarefas', component: TarefasComponent },
+  { path: 'orcamentos', component: ListaOrcamentosComponent },
+  { path: 'orcamentos/novo', component: NovoOrcamentoComponent },
+  { path: 'orcamentos/:id_orcamento', component: DetalheOrcamentoComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; // Importação para formulários reativos
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { ListaOrcamentosComponent } from './lista-orcamentos/lista-orcamentos.component';
+import { NovoOrcamentoComponent } from './novo-orcamento/novo-orcamento.component';
+import { DetalheOrcamentoComponent } from './detalhe-orcamento/detalhe-orcamento.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';  // Mantenha apenas o HttpClientModule
+import { OrcamentoService } from './services/orcamento.service';
+import { FormsModule } from '@angular/forms'; // Necessário para usar ngModel
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TarefasComponent,
+    ListaOrcamentosComponent,
+    NovoOrcamentoComponent,
+    DetalheOrcamentoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,  // Certifique-se de manter o HttpClientModule
+    FormsModule,       // Necessário para usar ngModel
+    ReactiveFormsModule // Importado caso precise de formulários reativos
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [OrcamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
